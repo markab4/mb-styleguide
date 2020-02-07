af4eaab346dafbdfe02e09d323e49751f5d792a3
Mark Abramov <markabramov01@gmail.com>
2020-02-07 13:59:50 -0500

    add size selector to typography

--- typography.js
+++ typography.js
@@ -54,11 +54,10 @@ function selectTypeface(typefaceElement) {
                 </div>`))
 }
 
-function selectStyle(styleElement){
+function selectStyle(styleElement) {
     $("#size-selector").remove();
     $("#style-selector").after(
-    $(`
-                <div class="type-selector-row _2" id="size-selector">
+        $(`                <div class="type-selector-row _2" id="size-selector">
                     <div class="typesel-header-with-toggle">
                         <div class="greyline"></div>
                         <div class="corpo20px">Size</div>
@@ -70,7 +69,7 @@ function selectStyle(styleElement){
                         </div>
                     </div>
                     <div class="div-block-76">
-                        <div class="typeface-selector-result active">
+                        <div class="typeface-selector-result selector active" onclick='selectSize(this)'>
                             <div>
                                 <div class="text-block-23">Master heading</div>
                             </div>
@@ -80,36 +79,20 @@ function selectStyle(styleElement){
                                 <div class="text-block-31">tracking <span class="specs">-10</span></div>
                             </div>
                         </div>
-                        <div class="typeface-selector-result">
+                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
                             <div class="text-block-25">Module heading</div>
-                            <div>
-                                <div class="text-block-31">size <span class="specs">32</span></div>
-                                <div class="text-block-31">leading <span class="specs">24</span></div>
-                                <div class="text-block-31">tracking <span class="specs">-10</span></div>
-                            </div>
                         </div>
-                        <div class="typeface-selector-result">
+                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
                             <div class="text-block-26">Paragraph heading</div>
-                            <div>
-                                <div class="text-block-31">size <span class="specs">32</span></div>
-                                <div class="text-block-31">leading <span class="specs">56</span></div>
-                                <div class="text-block-31">tracking <span class="specs">-10</span></div>
-                            </div>
                         </div>
-                        <div class="typeface-selector-result">
+                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
                             <div class="text-block-26">Paragraph heading</div>
-                            <div>
-                                <div class="text-block-31">size <span class="specs">32</span></div>
-                                <div class="text-block-31">leading <span class="specs">56</span></div>
-                                <div class="text-block-31">tracking <span class="specs">-10</span></div>
-                            </div>
                         </div>
                     </div>
-                </div>
-`))
-
+                </div>`));
+    $("#sample-design").remove();
     let $sampleDesign = $(`
-                <div class="sample-page-wrapper">
+                <div class="sample-page-wrapper" id="sample-design">
                     <div class="div-block-75"><img src="images/Image-682x.png"
                                                    srcset="images/Image-682x-p-500.png 500w, images/Image-682x-p-800.png 800w, images/Image-682x-p-1080.png 1080w, images/Image-682x-p-1600.png 1600w, images/Image-682x.png 2000w"
                                                    sizes="(max-width: 479px) 75vw, (max-width: 767px) 81vw, (max-width: 991px) 83vw, 69vw"
@@ -124,4 +107,11 @@ function selectStyle(styleElement){
                               alt=""></div>
                 </div>`);
     $("#design-sample-header").after($sampleDesign);
+}
+
+function selectSize(sizeElement) {
+    let $sizeSelector = $(".typeface-selector-result");
+    removeActive($sizeSelector);
+
+    toggleClasses(sizeElement, "active", "passive");
 }
