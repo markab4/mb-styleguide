d934a1217d623c3879d2a7088f98db657f98d125
Mark Abramov <markabramov01@gmail.com>
2020-02-10 11:05:40 -0500

    scroll in type

--- typography.js
+++ typography.js
@@ -57,7 +57,8 @@ function selectTypeface(typefaceElement) {
 function selectStyle(styleElement) {
     $("#size-selector").remove();
     $("#style-selector").after(
-        $(`                <div class="type-selector-row _2" id="size-selector">
+        $(`
+                <div class="type-selector-row _2" id="size-selector">
                     <div class="typesel-header-with-toggle">
                         <div class="greyline"></div>
                         <div class="corpo20px">Size</div>
@@ -69,43 +70,38 @@ function selectStyle(styleElement) {
                         </div>
                     </div>
                     <div class="div-block-76">
-                        <div class="typeface-selector-result selector active" onclick='selectSize(this)'>
+                        <a href="#master-heading" class="typeface-selector-result selector passive" onclick="selectSize(this)">
                             <div>
                                 <div class="text-block-23">Master heading</div>
                             </div>
-                            <div>
-                                <div class="text-block-31">size <span class="specs">32</span></div>
-                                <div class="text-block-31">leading <span class="specs">56</span></div>
-                                <div class="text-block-31">tracking <span class="specs">-10</span></div>
-                            </div>
-                        </div>
-                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
+                        </a>
+                        <a href="#module-heading"class="typeface-selector-result selector passive" onclick="selectSize(this)">
                             <div class="text-block-25">Module heading</div>
-                        </div>
-                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
+                        </a>
+                        <a href="#paragraph-heading" class="typeface-selector-result selector passive" onclick="selectSize(this)">
                             <div class="text-block-26">Paragraph heading</div>
-                        </div>
-                        <div class="typeface-selector-result selector passive" onclick='selectSize(this)'>
+                        </a>
+                        <a href="#paragraph-heading" class="typeface-selector-result selector passive" onclick="selectSize(this)">
                             <div class="text-block-26">Paragraph heading</div>
-                        </div>
+                        </a>
                     </div>
-                </div>`));
+                </div>
+`));
     $("#sample-design").remove();
     let $sampleDesign = $(`
                 <div class="sample-page-wrapper" id="sample-design">
                     <div class="div-block-75"><img src="images/Image-682x.png"
                                                    srcset="images/Image-682x-p-500.png 500w, images/Image-682x-p-800.png 800w, images/Image-682x-p-1080.png 1080w, images/Image-682x-p-1600.png 1600w, images/Image-682x.png 2000w"
-                                                   sizes="(max-width: 479px) 75vw, (max-width: 767px) 81vw, (max-width: 991px) 83vw, 69vw"
-                                                   alt=""></div>
+                                                   sizes="(max-width: 767px) 100vw, (max-width: 991px) 968px, 98vw"
+                                                   alt="" id="master-heading"></div>
                     <div><img src="images/Image-692x.png"
                               srcset="images/Image-692x-p-500.png 500w, images/Image-692x-p-800.png 800w, images/Image-692x-p-1080.png 1080w, images/Image-692x-p-1600.png 1600w, images/Image-692x.png 2000w"
-                              sizes="(max-width: 479px) 75vw, (max-width: 767px) 81vw, (max-width: 991px) 83vw, 69vw"
-                              alt=""></div>
+                              sizes="(max-width: 767px) 100vw, (max-width: 991px) 968px, 98vw" alt="" id="module-heading"></div>
                     <div><img src="images/Image-702x.png"
                               srcset="images/Image-702x-p-500.png 500w, images/Image-702x-p-800.png 800w, images/Image-702x-p-1080.png 1080w, images/Image-702x-p-1600.png 1600w, images/Image-702x.png 2000w"
-                              sizes="(max-width: 479px) 75vw, (max-width: 767px) 81vw, (max-width: 991px) 83vw, 69vw"
-                              alt=""></div>
-                </div>`);
+                              sizes="(max-width: 767px) 100vw, (max-width: 991px) 968px, 98vw" alt="" id="paragraph-heading"></div>
+                </div>
+`);
     $("#design-sample-header").after($sampleDesign);
 }
 
