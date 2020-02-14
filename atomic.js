8bb0cc22e18097d5cf057b3861a3dbdaf3ca5732
Mark Abramov <markabramov01@gmail.com>
2020-02-14 12:28:09 -0500

    switch out design samples based on user input in atoms

--- atomic.js
+++ atomic.js
@@ -1,10 +1,45 @@
 let selectedType, selectedStyle, selectedWidth;
 
-function initializeCtaOptions() {
+function switchDesignSamples() {
     selectedType = $("input[name='type']:checked").val();
     selectedStyle = $("input[name='style']:checked").val();
     selectedWidth = $("input[name='width']:checked").val();
-    console.log(selectedType, selectedStyle, selectedWidth);
+    console.log("selectedType:", selectedType, "\nselectedStyle:", selectedStyle, "\nselectedWidth:", selectedWidth);
+
+    let imgSource;
+
+    if (selectedType === "text-link") {
+        // Bold link
+        if (selectedStyle === "bold-link") imgSource = "images/Bold link.jpg";
+        // Primary text link
+        else if (selectedStyle === "primary-link") imgSource = "images/Primary  text link.jpg";
+        // Icon link
+        else
+        // if (selectedStyle === "icon-link")
+            imgSource = "images/Icon-Link.jpg";
+    } else {
+        // Primary button
+        if (selectedStyle === "primary") imgSource = "images/Primary Button.jpg";
+        // Secondary button
+        else if (selectedStyle === "secondary") imgSource = "images/Secondary Button.jpg";
+        // Secondary alternative
+        else
+        // if (selectedStyle === "secondary-alt")
+            imgSource = "images/Secondary Alternative.jpg";
+    }
+    $("#design-sample").remove();
+
+    $(".cta-example-wrapper").append($(`<img id="design-sample" src="${imgSource}" alt="">`));
+
+    console.log("imgSource", imgSource);
+}
+
+function initializeCtaOptions() {
+
+    selectedType = $("input[name='type']:checked").val();
+    selectedStyle = $("input[name='style']:checked").val();
+    selectedWidth = $("input[name='width']:checked").val();
+    console.log("selectedType:", selectedType, "\nselectedStyle:", selectedStyle, "\nselectedWidth:", selectedWidth);
 
     $(".cta-specs").remove();
     let $ctaSpecs;
@@ -18,42 +53,42 @@ function initializeCtaOptions() {
             </div>
             <div class="checkmarks-wrapper-vertical">
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" checked type="radio" id="primary" name="style"
                                value="primary">
                         <label class="radio_label" for="primary">Primary</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="secondary" name="style"
                                value="secondary">
                         <label class="radio_label" for="secondary">Secondary</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="secondary-alt" name="style"
                                value="secondary-alt">
                         <label class="radio_label" for="secondary-alt">Secondary Alt</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="tertiary" name="style"
                                value="tertiary">
                         <label class="radio_label" for="tertiary">Tertiary</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="AMG" name="style"
                                value="AMG">
                         <label class="radio_label" for="AMG">AMG</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="disabled" name="style"
                                value="disabled">
                         <label class="radio_label" for="disabled">Disabled</label><br>
@@ -67,12 +102,12 @@ function initializeCtaOptions() {
                 <div class="corpo20px">Width</div>
             </div>
             <div class="checkmarks-wrapper-vertical">
-                <div class="checkmarks-block buttons_selector w-clearfix">
+                <div class="checkmarks-block buttons_selector w-clearfix" onclick="switchDesignSamples()">
                     <input class="radio_button" checked type="radio" id="default" name="width"
                            value="default">
                     <label class="radio_label" for="default">Default</label><br>
                 </div>
-                <div class="checkmarks-block buttons_selector w-clearfix">
+                <div class="checkmarks-block buttons_selector w-clearfix" onclick="switchDesignSamples()">
                     <input class="radio_button" type="radio" id="wide" name="width"
                            value="width">
                     <label class="radio_label" for="wide">Wide</label><br>
@@ -91,42 +126,42 @@ function initializeCtaOptions() {
             </div>
             <div class="checkmarks-wrapper-vertical">
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" checked type="radio" id="primary-link" name="style"
                                value="primary-link">
                         <label class="radio_label" for="primary-link">Primary Link</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="secondary-link" name="style"
                                value="secondary-link">
                         <label class="radio_label" for="secondary-link">Secondary Link</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
-                        <input class="radio_button" type="radio" id="icon-link" name="style"
-                               value="icon-link">
-                        <label class="radio_label" for="icon-link">Icon Link</label><br>
-                    </div>
-                </div>
-                <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="bold-link" name="style"
                                value="bold-link">
                         <label class="radio_label" for="bold-link">Bold Link</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
+                        <input class="radio_button" type="radio" id="icon-link" name="style"
+                               value="icon-link">
+                        <label class="radio_label" for="icon-link">Icon Link</label><br>
+                    </div>
+                </div>
+                <div class="checkmarks-block buttons_selector">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="in-text-link" name="style"
                                value="in-text-link">
                         <label class="radio_label" for="in-text-link">In-text Link</label><br>
                     </div>
                 </div>
                 <div class="checkmarks-block buttons_selector">
-                    <div class="div-block-53 w-clearfix">
+                    <div class="div-block-53 w-clearfix" onclick="switchDesignSamples()">
                         <input class="radio_button" type="radio" id="in-text-icon-link" name="style"
                                value="in-text-icon-link">
                         <label class="radio_label" for="in-text-icon-link">In-text Icon Link</label><br>
@@ -139,12 +174,11 @@ function initializeCtaOptions() {
     }
     $(".cta-type").after($ctaSpecs);
 
+    switchDesignSamples(selectedType, selectedStyle, selectedWidth);
+
+
 }
 
-$(document).ready(function () {
+$(function () {
     initializeCtaOptions();
-    $("input[type='radio']").click(function () {
-        initializeCtaOptions();
-    });
-
 });
