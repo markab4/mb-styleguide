8f452eaa207160ebcc72d94a5ce3652d4e7408ca
Mark Abramov <markabramov01@gmail.com>
2020-02-18 14:26:47 -0500

    convert MB Brand Typesetting chart to JSON, update compliant colors

---
+++ typesetting.js
@@ -0,0 +1,276 @@
+let typesetting = {
+    "MBUSA": {
+        "Corporate A": {
+            "Regular": {
+                "desktop": {
+                    "Master Heading": {
+                        "size": "42px",
+                        "leading": "50px",
+                        "tracking": "0"
+                    },
+                    "Module Heading": {
+                        "size": "36px",
+                        "leading": "44px",
+                        "tracking": "0"
+                    },
+                    "Paragraph Heading": {
+                        "size": "30px",
+                        "leading": "36px",
+                        "tracking": "0"
+                    },
+                    "Navigational": {
+                        "size": "24px",
+                        "leading": "29px",
+                        "tracking": "0"
+                    },
+                    "Minimum": {
+                        "size": "18px",
+                        "leading": "24px",
+                        "tracking": "10"
+                    }
+                },
+                "mobile": {
+                    "Heading": {
+                        "size": "30px",
+                        "leading": "36px",
+                        "tracking": "0"
+                    },
+                    "Navigational": {
+                        "size": "24px",
+                        "leading": "29px",
+                        "tracking": "0"
+                    }
+                },
+            },
+            "Italic": {
+                "desktop": {
+                    "Callout": {
+                        "size": "24px",
+                        "leading": "29px",
+                        "tracking": "0"
+                    }
+                },
+                "mobile": {
+                    "Callout": {
+                        "size": "24px",
+                        "leading": "29px",
+                        "tracking": "0"
+                    }
+                }
+            }
+        },
+        "Corporate S Pro": {
+            "Medium": {
+                "desktop": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                },
+                "mobile": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                }
+            },
+            "Bold": {
+                "desktop": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                },
+                "mobile": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                }
+            }
+        },
+        "Arial": {
+            "Regular": {
+                "desktop": {
+                    "Body Copy": {
+                        "size": "13px",
+                        "leading": "20px",
+                        "tracking": "5"
+                    },
+                    "Minimum": {
+                        "size": "12px",
+                        "leading": "18px",
+                        "tracking": "10"
+                    }
+                },
+                "mobile": {
+                    "Body Copy": {
+                        "size": "13px",
+                        "leading": "20px",
+                        "tracking": "5"
+                    },
+                    "Minimum": {
+                        "size": "12px",
+                        "leading": "18px",
+                        "tracking": "10"
+                    }
+                }
+            }
+        },
+    },
+    "AMG": {
+        "Corporate A": {
+            "Italic": {
+                "desktop": {
+                    "Master Heading": {
+                        "size": "54px",
+                        "leading": "60px",
+                        "tracking": "0"
+                    },
+                    "Module Heading": {
+                        "size": "36px",
+                        "leading": "44px",
+                        "tracking": "0"
+                    },
+                    "Paragraph Heading": {
+                        "size": "30px",
+                        "leading": "36px",
+                        "tracking": "0"
+                    },
+                    "Navigational": {
+                        "size": "24px",
+                        "leading": "29px",
+                        "tracking": "0"
+                    },
+                    "Minimum": {
+                        "size": "18px",
+                        "leading": "24px",
+                        "tracking": "10"
+                    }
+                },
+                "mobile": {
+                    "Heading": {
+                        "size": "42px",
+                        "leading": "48px",
+                        "tracking": "0"
+                    },
+                    "Navigational": {
+                        "size": "30px",
+                        "leading": "36px",
+                        "tracking": "0"
+                    }
+                },
+            },
+        },
+        "Corporate S Pro": {
+            "Medium": {
+                "desktop": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                },
+                "mobile": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                }
+            },
+            "Bold": {
+                "desktop": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                },
+                "mobile": {
+                    "Large All Caps": {
+                        "size": "18px",
+                        "leading": "22px",
+                        "tracking": "20"
+                    },
+                    "Navigation All Caps": {
+                        "size": "14px",
+                        "leading": "18px",
+                        "tracking": "20"
+                    }
+                }
+            }
+        },
+        "Arial": {
+            "Regular": {
+                "desktop": {
+                    "Body Copy": {
+                        "size": "13px",
+                        "leading": "20px",
+                        "tracking": "5"
+                    },
+                    "Minimum": {
+                        "size": "12px",
+                        "leading": "18px",
+                        "tracking": "10"
+                    }
+                },
+                "mobile": {
+                    "Body Copy": {
+                        "size": "13px",
+                        "leading": "20px",
+                        "tracking": "5"
+                    },
+                    "Minimum": {
+                        "size": "12px",
+                        "leading": "18px",
+                        "tracking": "10"
+                    }
+                }
+            }
+        },
+    },
+    "VANS": {},
+    "EQ": {},
+
+
+};
